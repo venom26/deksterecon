d47280d4ab53e0110348291784fe2bad9db3cce8
venom26 <54362015+venom26@users.noreply.github.com>
2020-10-15 19:19:53 +0530

    Update dekster.sh

--- dekster.sh
+++ dekster.sh
@@ -4,7 +4,7 @@ echo "$(tput setaf 2)Running Automation to gather data on" $1
 
 mkdir /var/www/html/$1-$3
 
-findomain -t $1 -u /var/www/html/$1-$3/$1-subs.txt
+/usr/local/bin/findomain -t $1 -u /var/www/html/$1-$3/$1-subs.txt
 subfinder -d $1 -recursive | tee -a /var/www/html/$1-$3/$1-subs.txt
 curl -ss https://dns.bufferover.run/dns?q=.$1 | jq '.FDNS_A[]' | sed 's/^\".*.,//g' | sed 's/\"$//g'  | sort -u | tee -a /var/www/html/$1-$3/$1-subs.txt
 amass enum -passive -d $1 | tee -a /var/www/html/$1-$3/$1-subs.txt 
@@ -29,7 +29,7 @@ fi
 
 if [[ "$2" == "full_scan" ]]
 then
-python3 dirsearch/dirsearch.py -l /var/www/html/$1-$3/$1-subdomains.txt -e php,html,json -w ./dirsearch/db/dicc.txt -b -t 100 -x 301,400,400,429,307,503,500,305,412,502 --plain-text-report=/var/www/html/$1-$3/$1-dirsearch.txt
+python3 dirsearch/dirsearch.py -l /var/www/html/$1-$3/$1-subdomains.txt -e php,html -w ./dirsearch/db/dicc.txt -b -t 150 -x 301,400,401,429,307,503,500,305,412,502 --plain-text-report=/var/www/html/$1-$3/$1-dirsearch.txt
 cat /var/www/html/$1-$3/$1-subdomains.txt | httpx -threads 200 -status-code -title -json -o /var/www/html/$1-$3/$1-Httpx-output.json
 cat /var/www/html/$1-$3/$1-subdomains.txt | aquatone -out /var/www/html/$1-$3/$1-aqua-out
 sed -e 's|^[^/]*//||' -e 's|/.*$||' /var/www/html/$1-$3/$1-subdomains.txt | naabu | tee -a /var/www/html/$1-$3/ports-$1.txt
